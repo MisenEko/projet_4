8b27a6e91fff938a2d2da58867998004e11d53ac
MisenEko <48181258+MisenEko@users.noreply.github.com>
2020-03-12 21:44:12 +0100

    minimum changes  nothing important

---
+++ model/manager.php
@@ -0,0 +1,3 @@
+<?php
+
+$db = new \PDO("mysql:host=localhost;dbname=forteroche;charset=utf-8", 'root', '' );
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -8,5 +8,6 @@
         <H3>TEST</H3>
     </body>
 
-
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
