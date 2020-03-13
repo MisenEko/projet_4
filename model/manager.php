6a7698d1ec97c98fe78139c011f3b99c2b423cfb
MisenEko <48181258+MisenEko@users.noreply.github.com>
2020-03-13 17:02:31 +0100

    Changes on different files
    many errors, but I know why, I need to use a controller. I'll do it on my main computer.

--- model/manager.php
+++ model/manager.php
@@ -1,3 +1,12 @@
 <?php
 
-$db = new \PDO("mysql:host=localhost;dbname=forteroche;charset=utf-8", 'root', '' );
+class Manager{
+
+    protected function dbConnect(){
+
+    $db = new \PDO("mysql:host=localhost;dbname=forteroche;charset=utf-8", 'root', '' );
+    return $db;
+    
+    }
+
+}
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -1,3 +1,13 @@
+<?php 
+
+require("view/frontend/head.php");
+require("model/showPost.php");
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,8 +16,18 @@
 
     <body>
         <H3>TEST</H3>
+
+        </br>
+
+        <?php
+
+        $reponse = getPosts();
+        ?>
+
+        <h3>Test de titre : <?php echo $reponse['title']; ?></h3>
     </body>
 
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
+
