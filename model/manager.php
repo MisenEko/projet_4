aeba711b4a141eb80ea7e9964cb8ac8692df79ba
MisenEko <48181258+MisenEko@users.noreply.github.com>
2020-03-21 13:29:56 +0100

    push the last version who works well

--- model/manager.php
+++ model/manager.php
@@ -5,10 +5,7 @@ class Manager{
     protected function dbConnect(){
 
     $db = new PDO("mysql:host=localhost;dbname=forteroche;charset=utf8", 'root', '' );
-    return $db;
-
-    
-    
+    return $db;    
     }
 
 }
